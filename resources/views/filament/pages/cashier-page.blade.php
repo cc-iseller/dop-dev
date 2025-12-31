345ae8df6347ff55744629b84d1de755ac845deb
zeinkurnia <kurniaadika5@gmail.com>
2025-12-31 10:31:19 +0700

    kasir-page

--- resources/views/filament/pages/cashier-page.blade.php
+++ resources/views/filament/pages/cashier-page.blade.php
@@ -1,3 +1,20 @@
 <x-filament-panels::page>
-    {{-- Page content --}}
+    <div class="grid grid-cols-12 gap-4">
+    <x-filament::section icon="heroicon-o-shopping-bag"  class="col-span-8">
+        <x-slot name="heading">
+            Produk Tersedia
+        </x-slot>
+
+        @livewire('cashier-page')
+        @vite(['resources/css/app.css', 'resources/js/app.js'])  
+    </x-filament::section>
+
+    <x-filament::section icon="heroicon-o-shopping-cart" class="col-span-4">
+        <x-slot name="heading">
+            Keranjang Belanja
+        </x-slot>
+        
+        @vite(['resources/css/app.css', 'resources/js/app.js'])  
+        <div id="cart-section"></div>
+    </x-filament::section>
 </x-filament-panels::page>
--- resources/views/filament/pages/pelaporan-page.blade.php
+++ resources/views/filament/pages/pelaporan-page.blade.php
@@ -1,3 +1,24 @@
 <x-filament-panels::page>
-    {{-- Page content --}}
+    <x-filament::section icon="heroicon-o-presentation-chart-line"  class="col-span-8">
+        <x-slot name="heading">
+            Analisis Pelaporan
+        </x-slot>
+
+        @livewire('pelaporan-page')
+        @vite(['resources/css/app.css', 'resources/js/app.js'])  
+    </x-filament::section>
+
+    <x-filament::section icon="heroicon-o-banknotes"  class="col-span-8">
+        <x-slot name="heading">
+            Produk Terlaris
+        </x-slot>
+        @vite(['resources/css/app.css', 'resources/js/app.js'])  
+    </x-filament::section>
+
+    <x-filament::section icon="heroicon-o-banknotes"  class="col-span-8">
+        <x-slot name="heading">
+            Transaksi Harian
+        </x-slot>
+        @vite(['resources/css/app.css', 'resources/js/app.js'])  
+    </x-filament::section>
 </x-filament-panels::page>
